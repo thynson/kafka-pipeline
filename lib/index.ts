523b7627fdc023184af6f152d216557d5e6c2bfc
LAN Xingcan <thynson@live.com>
2019-04-17 17:07:37 +0800

    Export missing interfaces

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,3 @@
+export {MessageConsumer, FailedMessageConsumer, ConsumeOption} from './consume-stream';
 export {default as ConsumeTimeoutError} from './consume-timeout-error';
 export {default, ConsumerGroupPipeline } from './consumer-group-pipeline';
