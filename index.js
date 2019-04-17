293feb7b224c018a7de55a6f6a9e99f36aefed35
LAN Xingcan <thynson@live.com>
2019-04-17 10:22:11 +0800

    Remove unused index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const {ConsumerGroupPipeline} = require('./lib/consumer-group-pipeline');
-const {ConsumeTimeoutError} = require('./lib/consume-timeout-error');
-module.exports = {
-  ConsumeTimeoutError,
-  ConsumerGroupPipeline
-};
